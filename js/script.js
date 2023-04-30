94f0b2ef727890d35771af1280114595ed5c1515
Davi Felix <davifmatias2022@gmail.com>
2023-04-30 14:57:10 -0300

    adicionado janela modal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
-function add_task(){
+let btn_add = document.getElementById("add");
+
+btn_add.addEventListener('click', function(){
     let input = document.getElementById("input_task");
     
-    const task = document.createElement("div");
-    const container = document.querySelector(".container");
-    const p = document.createElement("p");
-    const img = document.createElement("img");
-
-    let paragraph = input.value;
-    p.innerHTML = paragraph;
-
-    img.src = "../img/trash.png"
+    if(input.value != ''){
+        const task = document.createElement("div");
+        let container = document.querySelector(".container");
+        let p = document.createElement("p");
+        const img = document.createElement("img");
+ 
+        let paragraph = input.value;
+        p.innerHTML = paragraph;
+ 
+        img.src = "../img/trash.png";
+        task.className = "tasks";
+        img.className = "trash";
+ 
+        container.appendChild(task);
+        task.appendChild(p);
+        task.appendChild(img);
     
-    task.className = "tasks";
-    img.className = "trash";
+        img.addEventListener('click',function(){
+            container.removeChild(task);
+        })
+ 
+        input.value = '';
+   }else{
+        let modal = document.querySelector("dialog");
+        let btn_close = document.querySelector("#close");
 
-    container.appendChild(task);
-    task.appendChild(p);
-    task.appendChild(img);
-}
+        modal.showModal();
+        btn_close.onclick = function(){
+            modal.close();
+        }
+   }    
+});
+
